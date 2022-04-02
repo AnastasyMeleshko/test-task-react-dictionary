a733d241135b7cfb6a2edc7416e5f34436555804
AnastasyMeleshko <7634907@mail.ru>
2022-04-02 22:58:58 +0300

    added draft markup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import "./App.css";
+import HomePage from "./components/HomePage/HomePage";
+import InfoPage from "./components/InfoPage/InfoPage";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Header/>
+            <br/>
+            <br/>
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/info" element={<InfoPage/>}/>
+            </Routes>
+            <br/>
+            <br/>
+            <br/>
+            <Footer/>
+        </Router>
+    );
+};
 
 export default App;
---
+++ src/components/InfoPage/InfoPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const InfoPage = () => {
+    return (
+        <main>
+            InfoPage
+        </main>
+    );
+};
+
+export default InfoPage;
